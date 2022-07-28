85779ead59b9f870b4644bf9dd415e73b2af08e2
austin <aokojie@gmail.comclear>
2022-07-28 09:43:29 -0400

    updated all files

--- Start With Terraform Basics/MyEC2InstanceTerraform.tf
+++ Start With Terraform Basics/MyEC2InstanceTerraform.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-    access_key = "AKIAWGNNRZS3FXOO6A7O"
-    secret_key = "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"
-    region     = "us-east-2"
-}
-
 resource "aws_instance" "MyTerrafEC2" {
   count         = 3
   ami           = "ami-02d1e544b84bf7502"
