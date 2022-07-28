3b55e22b8f6e90f305b49cb3033d5bc7ebcade9f
austin <aokojie@gmail.comclear>
2022-07-28 09:42:41 -0400

    updated files

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAWGNNRZS3FXOO6A7O"
-  secret_key = "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"
-  alias      = "east"
-  region     = "us-east-2"
+    version    = "4.22.0" 
+    access_key = "AKIAWGNNRZS3FXOO6A7O"
+    secret_key = "SECRET+KEY_HERE"
+    region     = "us-east-2"
 }
