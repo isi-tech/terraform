d32f1e3002668dc70ead7ef169eb0ac2d77c9986
austin <aokojie@gmail.comclear>
2022-07-28 12:42:53 -0400

    added the key

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     version    = "4.22.0"
     access_key = "AKIAWGNNRZS3FXOO6A7O"
-    secret_key = "SECRET+KEY_HERE"
+    secret_key = "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"
     region     = "us-east-2"
 }
