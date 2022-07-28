511490d470de0cf49efb8cc66d41b354632551ab
austin <aokojie@gmail.comclear>
2022-07-28 12:48:31 -0400

    merged tow files

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,6 +1,15 @@
+## Provider's Example
+
+# Configure the AWS Provider
 provider "aws" {
     version    = "4.22.0"
     access_key = "AKIAWGNNRZS3FXOO6A7O"
-    secret_key = "5+GU/sF244F7XKK2wznioaACdHixtznXbY4SWAsW"
+    secret_key = "SECRET+KEY_HERE"
     region     = "us-east-2"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    version = "3.15.0"
+    features {}
 }
